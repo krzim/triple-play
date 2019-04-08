cf77272aa5dbad5bfe9113140c7a207a6ae04ca5
Hannah Burke <burke_hannah@bah.com>
2019-04-08 09:06:52 -0400

    Hello

--- testing/api_gateway/test_workflows.py
+++ testing/api_gateway/test_workflows.py
@@ -5,12 +5,15 @@ import json
 import logging
 # def test_abort_workflow(api_gateway, workflow, execdb):
 
-def test_workflow_exist_post(api_gateway, token, workflow, execdb):
+def test_workflow_exist_post(api_gateway, token, execdb):
 	header = {'Authorization': 'Bearer {}'.format(token['access_token'])}
-	SUCCESS = 200
+	SUCCESS = 201
 	with open("testing/util/workflow.json") as fp:
 		wf_json = json.load(fp)
 		data = json.dumps(wf_json)
-	
-	response = api_gateway.post('/api/workflows', data=data, headers=header, content_type="application/json")
-	assert response.status_code == SUCCESS
+
+	response1 = api_gateway.post('/api/workflows', data=data, headers=header, content_type="application/json")
+	assert response1.status_code == SUCCESS
+	response2 = api_gateway.get('/api/workflows', data={"workflow_id": "e8c7840a-bd3e-4cfd-b9be-e07269b10c89"}, headers=header, content_type="application/json")
+	key = json.loads(response2.get_data(as_text=True))
+		
